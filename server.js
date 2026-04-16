427ef42176252d55cef7e1469ac552aa245c5106
Montyyy <danyramadhan558@gmail.com>
2026-04-16 19:59:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express');
-const fs = require('fs'); // 1. Memanggil Pustakawan
+const fs = require('fs'); 
 
 const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-// 2. Menentukan letak Buku Catatan
+
 const FILE_DATABASE = './database.json';
+
+const express = require('express');
+const fs = require('fs'); 
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+
+const FILE_DATABASE = './database.json';
+
+app.get('/jurnal', (req, res) => {
+    try {
+       
+        const dataTeks = fs.readFileSync(FILE_DATABASE, 'utf-8');
+        
+       
+        const dataJurnal = JSON.parse(dataTeks);
+
+        
+        res.json({ pesan: "Berhasil mengambil data!", data: dataJurnal });
+    } catch (error) {
+        res.status(500).json({ pesan: "Gagal mengambil data!" });
+    }
+});
