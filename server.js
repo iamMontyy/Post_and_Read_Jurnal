e66a9a0242f1359cafbfa6784d37a48e210f6917
Montyyy <danyramadhan558@gmail.com>
2026-04-16 20:00:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ app.use(express.json());
 
 const FILE_DATABASE = './database.json';
 
-const express = require('express');
-const fs = require('fs'); 
-
-const app = express();
-const PORT = 3000;
-app.use(express.json());
-
-
-const FILE_DATABASE = './database.json';
-
 app.get('/jurnal', (req, res) => {
     try {
        
@@ -27,8 +17,8 @@ app.get('/jurnal', (req, res) => {
         const dataJurnal = JSON.parse(dataTeks);
 
         
-        res.json({ pesan: "Berhasil mengambil data!", data: dataJurnal });
+        res.json({ pesan: "Berhasil mengambil data! jurnal", data: dataJurnal });
     } catch (error) {
-        res.status(500).json({ pesan: "Gagal mengambil data!" });
+        res.status(500).json({ pesan: "Gagal mengambil data jurnal!" });
     }
 });
