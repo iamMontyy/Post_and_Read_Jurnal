22f7ae9efbc6351bfeb512c68ff633d7926719fd
Montyyy <danyramadhan558@gmail.com>
2026-04-18 19:22:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,3 +22,31 @@ app.get('/jurnal', (req, res) => {
         res.status(500).json({ pesan: "Gagal mengambil data jurnal!" });
     }
 });
+
+app.post('/jurnal', (req, res) => {
+    try {
+        const { judul, isi } = req.body;
+
+        
+        const dataTeks = fs.readFileSync(FILE_DATABASE, 'utf-8');
+        const dataJurnal = JSON.parse(dataTeks);
+
+        
+        const jurnalBaru = {
+            id: Date.now(), 
+            tanggal: new Date().toLocaleDateString('id-ID'),
+            judul: judul,
+            isi: isi
+        };
+
+        
+        dataJurnal.push(jurnalBaru);
+
+    
+        fs.writeFileSync(FILE_DATABASE, JSON.stringify(dataJurnal, null, 2));
+
+        res.status(201).json({ pesan: "Jurnal berhasil disimpan permanen!", data: jurnalBaru });
+    } catch (error) {
+        res.status(500).json({ pesan: "Gagal menyimpan jurnal." });
+    }
+});
